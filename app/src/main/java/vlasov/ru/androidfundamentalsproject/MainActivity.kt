a21d59ad5ee25a4153069b53d015d1a9fbdae1bd
Egor <giksengik@gmail.com>
2021-06-02 09:54:59 +0300

    first hw

---
+++ app/src/main/java/vlasov/ru/androidfundamentalsproject/MainActivity.kt
@@ -0,0 +1,12 @@
+package vlasov.ru.androidfundamentalsproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.movie_details)
+
+    }
+}
